f42160bbfee9afff8fde090c1d02499eca2d127b
Todd Ornett <toddgh@acquirus.com>
2026-09-10 14:22:43 +0900

    feat: add ghost-teams.sh

---
+++ ghost-teams.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+panel="3"
+while true; do
+  osascript -e 'tell application "Microsoft Teams" to activate'
+  osascript -e "tell application \"System Events\" to keystroke \"$panel\" using {control down}"
+  panel=$((panel + 1))
+  if [[ "$panel" -gt "3" ]]; then
+    panel="1"
+  fi
+  echo "Teams Status Refreshed"
+  sleep 300
+done
