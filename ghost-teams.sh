192b278c96ae22361a7e82b549c06f9f94fdf541
Todd Ornett <toddgh@acquirus.com>
2026-09-11 10:34:58 +0900

    fix: ghost-teams.sh use command key modifier

--- ghost-teams.sh
+++ ghost-teams.sh
@@ -2,7 +2,7 @@
 panel="3"
 while true; do
   osascript -e 'tell application "Microsoft Teams" to activate'
-  osascript -e "tell application \"System Events\" to keystroke \"$panel\" using {control down}"
+  osascript -e "tell application \"System Events\" to keystroke \"$panel\" using {command down}"
   panel=$((panel + 1))
   if [[ "$panel" -gt "3" ]]; then
     panel="1"
